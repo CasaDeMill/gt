b3319670cbe402a731f1acb294946e9881dcc698
Martin Lindgren <hi@martinlindgren.com>
2025-10-22 22:24:08 +0200

    Please

--- src/routes/api/reddit/+server.ts
+++ src/routes/api/reddit/+server.ts
@@ -6,13 +6,13 @@ export const POST: RequestHandler = async ({request}) => {
   const returnArr: any[] = [];
   for (let index = 0; index < subReddits.length; index++) {
     const element = subReddits[index];
-    const result = await fetch(`https://www.reddit.com/r/${element}/new.json`,
-      {
-        headers: {
-          'User-Agent': 'Fuck off'
-        } 
-      }
-    );
+    const options = {
+      headers: {
+        'User-Agent': 'Fuck off'
+      } 
+    };
+    console.log(options);
+    const result = await fetch(`https://www.reddit.com/r/${element}/new.json`, options);
     console.log("result:", result);
     const jsonResponse = await result.json();
     const simpleData = jsonResponse.data.children.map((c: any) => ({
