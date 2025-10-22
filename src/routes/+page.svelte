0c305a1f5bfdc24893a92c3641d2568fb36cc938
Martin Lindgren <hi@martinlindgren.com>
2025-10-22 21:49:03 +0200

    Rewrite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,196 @@
 <script lang="ts">
-	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-  import '@splidejs/splide/dist/css/themes/splide-default.min.css';
-
   let imageData: any[] = $state([]);
+  let selectedIndex: number = $state(0);
+  let selectedSubs: string[] = $state([]);
+  let avSubs = $state(['HalfLife', 'Panties']);
+  let newSub = $state('');
+  let loading = $state(false);
 
-const getData = async () => {
-  const response = await fetch('/api/reddit', { method: 'GET' });
+  const shuffle = (array: any[]) => {
+    let currentIndex = array.length;
 
-	imageData = await response.json();
-}
+    while (currentIndex != 0) {
+
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  }
+
+  const getData = async () => {
+    loading = true;
+    const response = await fetch('/api/reddit', { method: 'POST',
+			body: JSON.stringify(selectedSubs),
+			headers: {
+				'Content-Type': 'application/json'
+			} });
+    var responseArr = await response.json();
+    loading = false;
+    shuffle(responseArr);
+    imageData = responseArr;
+  }
+
+  const incIndex = () => {
+    if (selectedIndex < imageData.length - 1) {
+      selectedIndex++;
+    }
+  }
+
+  const decIndex = () => {
+    if (selectedIndex >= 1) {
+      selectedIndex--;
+    }
+  }
+
+  const addNew = () => {
+    avSubs.push(newSub);
+    selectedSubs.push(newSub);
+    newSub = "";
+  }
 </script>
 
 <div class="wrapper">
-  <button onclick={getData}>GO</button>
-  <Splide options={{ perPage: 1, gap: "1rem" }}>
-    {#each imageData as image}
-        {#if image.urls.length > 1}
-          <Splide options={{ perPage: 1, gap: "1rem" }}>
-            {#each image.urls as url}
-              <SplideSlide>
-                {#if url.endsWith("jpg") || url.endsWith("png") || url.endsWith("jpeg")}
-                  <img width={100} src={url}/>
-                  {:else}
-                  <video width={100} src={url}/>
-                {/if}
-              </SplideSlide>
-            {/each}
-          </Splide>
-        {:else}
-          <SplideSlide>
-            {#if image.urls[0].endsWith("jpg") || image.urls[0].endsWith("png") | image.urls[0].endsWith("jpeg")}
-              <img width={100} src={image.urls[0]}/>
-            {:else}
-              <video width={100} src={image.urls[0]}/>
-            {/if}
-          </SplideSlide>
-        {/if}
-    {/each}
-  </Splide>
+  {#if imageData.length > 0}
+    <div>
+      {#if
+        imageData[selectedIndex].src.endsWith("jpg")
+        || imageData[selectedIndex].src.endsWith("png")
+        || imageData[selectedIndex].src.endsWith("jpeg")
+        || imageData[selectedIndex].src.endsWith("gif")
+      }
+        <!-- svelte-ignore a11y_missing_attribute -->
+        <img class="image" src={imageData[selectedIndex].src}/>
+      {:else if imageData[selectedIndex].src.includes("youtu.be")}
+        <!-- svelte-ignore a11y_missing_attribute -->
+        <iframe class="yotube" src={`https://www.youtube.com/embed/${imageData[selectedIndex].src.split('/').pop()}`}></iframe>
+      {:else}
+        <!-- svelte-ignore a11y_media_has_caption -->
+        <video class="video" src={imageData[selectedIndex].src} autoplay controls></video>
+      {/if}
+      <div class="buttonContainer">
+        <button onclick={decIndex}>Previous</button>
+        <button onclick={incIndex}>Next</button>
+      </div>
+    </div>
+  {:else}
+    <div class="setupContainer">
+      <h1>Okeeeeej</h1>
+      <input bind:value={newSub}>
+      <button onclick={addNew}>
+        Add
+      </button>
+      {#each avSubs as sub}
+        <label>
+          <input
+            type="checkbox"
+            name="sub"
+            value={sub}
+            bind:group={selectedSubs}
+          />
+
+          {sub}
+        </label>
+      {/each}
+      <button onclick={getData}>
+        GO
+      </button>
+      {#if loading}
+        <progress></progress>
+      {/if}
+    </div>
+  {/if}
 </div>
+
 
 <style>
   .wrapper {
-    max-width: 600px;
-    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100dvh;
+  }
+  .buttonContainer {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+  }
+  .setupContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    height: 100dvh;
+  }
+  .youtube {
+    width: 90dvw;
+    border-radius: 5px;
+  }
+  .image {
+    width: 90dvw;
+    border-radius: 5px;
+  }
+  .video {
+    width: 90dvw;
+    border-radius: 5px;
+  }
+  button,
+  button:focus {
+    user-select: none;
+    font-size: 17px;
+    padding: 10px 25px;
+    border-radius: 0.7rem;
+    background-image: linear-gradient(rgb(214, 202, 254), rgb(158, 129, 254));
+    border: 2px solid rgb(50, 50, 50);
+    border-bottom: 5px solid rgb(50, 50, 50);
+    box-shadow: 0px 1px 6px 0px rgb(158, 129, 254);
+    transform: translate(0, -3px);
+    cursor: pointer;
+    transition: 0.2s;
+    transition-timing-function: linear;
+  }
+
+  button:active {
+    user-select: none;
+    transform: translate(0, 0);
+    border-bottom: 2px solid rgb(50, 50, 50);
+  }
+
+  h1 {
+    font-family: "Kinkee";
+  }
+
+  p {
+    width: 75%;
+  }
+
+  p,
+  span,
+  button,
+  input,
+  div {
+    font-family: "Georgia";
+    color: #330a47;
+  }
+
+  input[type="checkbox"] {
+    accent-color: #8634af;
+    width: 25px;
+    height: 25px;
+  }
+
+  input {
+    border-radius: 5px;
+    font-size: 18px;
+    width: 200px;
+    margin-top: 10px;
+  }
+
+  progress {
+    width: 100%;
+    accent-color: #330a47;
   }
 </style>
--- src/routes/api/reddit/+server.ts
+++ src/routes/api/reddit/+server.ts
@@ -1,26 +1,50 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async () => {
-  const result = await fetch("https://www.reddit.com/r/HalfLife/new.json");
-  const jsonResponse = await result.json();
-  const simpleData = jsonResponse.data.children.map((c: any) => ({
-      id: c.data.id,
-      gallery: c.data.is_gallery,
-      video: c.data.is_video,
-      spoiler: c.data.spoiler,
-      over18: c.data.over_18,
-      urls: c.data.is_video
-        ? [c.data.secure_media.reddit_video.fallback_url]
-        : c.data.is_gallery
-          ? Object.keys(c.data.media_metadata).map(
-              (m) =>
-                `https://i.redd.it/${m}.${c.data.media_metadata[m].m.split("/")[1]}`,
-            )
-          : c.data.spoiler
-            ? [c.data.url]
-            : [c.data.url],
-    }));
+export const POST: RequestHandler = async ({request}) => {
+  const subReddits: string[] = await request.json();
+  const returnArr: any[] = [];
+  for (let index = 0; index < subReddits.length; index++) {
+    const element = subReddits[index];
+    const result = await fetch(`https://www.reddit.com/r/${element}/new.json`);
+    const jsonResponse = await result.json();
+    const simpleData = jsonResponse.data.children.map((c: any) => ({
+        id: c.data.id,
+        gallery: c.data.is_gallery,
+        video: c.data.is_video,
+        spoiler: c.data.spoiler,
+        over18: c.data.over_18,
+        urls: c.data.is_video
+          ? [c.data.secure_media.reddit_video.fallback_url]
+          : c.data.is_gallery
+            ? Object.keys(c.data.media_metadata).map(
+                (m) =>
+                  `https://i.redd.it/${m}.${c.data.media_metadata[m].m.split("/")[1]}`,
+              )
+            : c.data.spoiler
+              ? [c.data.url]
+              : [c.data.url],
+      }));
 
-  return json(simpleData.filter((s: any) => s.urls.every((u: any) => !u.endsWith("/"))));
+    const media = simpleData.filter((s: any) => s.urls.every((u: any) => !u.endsWith("/")));
+
+    media.forEach((element: any) => {
+      if (element.urls.length > 1) {
+        element.urls.forEach((url: any) => {
+          returnArr.push({
+            id: element.id,
+            src: url
+          });
+        });
+      }
+      else {
+        returnArr.push({
+          id: element.id,
+          src: element.urls[0]
+        });
+      }
+    });
+  }
+
+  return json(returnArr);
 };
